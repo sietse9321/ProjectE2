2bb534bea6830e1f2e7023f99eef6488fabfdfbe
sietse <0335816@student.rocvantwente.nl>
2022-05-11 09:57:35 +0200

    Script
    made scripts

---
+++ Assets/Scripts/LoadObjects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadObjects : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject MainCanvas;
+    public GameObject BackGound;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
