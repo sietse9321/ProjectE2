93acfe074355df96324a123a1011624da6b5e046
sietse <0335816@student.rocvantwente.nl>
2022-05-12 13:22:13 +0200

    script
    >???

--- Assets/Scripts/ButtonAction.cs
+++ Assets/Scripts/ButtonAction.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ButtonAction : MonoBehaviour
 {
+    //public gameobject manager zodat die kan spreken met andere objecten
     public GameObject Manager;
     public void RestartButton()
     {
+        //laad scene " . . . "
         SceneManager.LoadScene(0);
     }
 }
